5c72c767f30bed8d9ff7fa9b257b8ac5009810ea
Somanshu <somanshuprasad@gmail.com>
2022-06-30 02:14:13 -0400

    Updated imdb calls toinclude retry

--- imdb.py
+++ imdb.py
@@ -7,14 +7,15 @@ import time
 class imdb(object):
     HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
 
+    def __init__(self):
+        self.s = requests.Session()
+        retries = Retry(total=10, backoff_factor=1, status_forcelist=[429,500,502,503,504])
+        self.s.mount('http://', HTTPAdapter(max_retries=retries))
+
     # Identify if media is TV show or movie
     def identify_media(self,media_id):
-        s = requests.Session()
-        retries = Retry(total=5, backoff_factor=1, status_forcelist=[429,500,502,503,504])
-        s.mount('http://', HTTPAdapter(max_retries=retries))
-
         url = fr"https://www.imdb.com/title/{media_id}/"
-        response = s.get(url, headers=self.HEADERS)
+        response = self.s.get(url, headers=self.HEADERS)
 
 
         soup2 = BeautifulSoup(response.text,features="lxml")
@@ -27,7 +28,7 @@ class imdb(object):
     # Call and scrape imdb list
     def scraping_imdb_list(self):
         imdb_url = "https://www.imdb.com/list/ls500310832/"
-        imdb_response = requests.get(imdb_url, headers=self.HEADERS)
+        imdb_response = self.s.get(imdb_url, headers=self.HEADERS)
         soup = BeautifulSoup(imdb_response.text,features="lxml") 
         raw_list = json.loads(soup.find_all('script',type='application/ld+json')[0].string)
         imdb_id_list = [imdb_id["url"].split(r"/")[-2] for imdb_id in raw_list["about"]["itemListElement"]]
